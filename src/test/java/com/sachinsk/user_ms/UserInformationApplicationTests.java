c26e43fdecd365f2ebffe358f74660ecd73eccac
Sachin <sachinkaushikofficial@gmail.com>
2025-03-30 23:01:28 +0530

    Initial commit

---
+++ src/test/java/com/sachinsk/user_ms/UserInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachinsk.user_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
