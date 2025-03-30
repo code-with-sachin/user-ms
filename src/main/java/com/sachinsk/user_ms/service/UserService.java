2e57980e25d99a4264f597ffc0e5c434d3f525d1
Sachin <sachinkaushikofficial@gmail.com>
2025-03-30 23:49:48 +0530

    added Save user logic

--- src/main/java/com/sachinsk/user_ms/service/UserService.java
+++ src/main/java/com/sachinsk/user_ms/service/UserService.java
@@ -1,5 +1,8 @@
 package com.sachinsk.user_ms.service;
 
+import com.sachinsk.user_ms.dto.UserDTO;
+import com.sachinsk.user_ms.entity.User;
+import com.sachinsk.user_ms.mapper.UserMapper;
 import com.sachinsk.user_ms.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +12,13 @@ public class UserService {
 
     @Autowired
     UserRepo userRepo;
+
+    public UserDTO addUserToDB(UserDTO userDTO) {
+
+        //mapping DTO to entity to save to DB : since we save entity to DB and Not the DTO
+        User savedUser = userRepo.save(UserMapper.INSTANCE.mapUserDTOToUser(userDTO));
+
+        //mapping entity back to DTO since we are returning the DTO to enduser and not the real entity
+        return UserMapper.INSTANCE.mapUserToUserDTO(savedUser);
+    }
 }
