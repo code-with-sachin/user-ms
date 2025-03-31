b778ea446803b29193afa44ef793cd3d7556a658
Sachin <sachinkaushikofficial@gmail.com>
2025-03-31 12:22:18 +0530

    modularize the service code

--- src/main/java/com/sachinsk/user_ms/service/UserService.java
+++ src/main/java/com/sachinsk/user_ms/service/UserService.java
@@ -1,31 +1,10 @@
 package com.sachinsk.user_ms.service;
 
 import com.sachinsk.user_ms.dto.UserDTO;
-import com.sachinsk.user_ms.entity.User;
-import com.sachinsk.user_ms.mapper.UserMapper;
-import com.sachinsk.user_ms.repository.UserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
+public interface UserService {
 
-    @Autowired
-    UserRepo userRepo;
+    public UserDTO addUserToDB(UserDTO userDTO);
 
-    public UserDTO addUserToDB(UserDTO userDTO) {
-
-        //mapping DTO to entity to save to DB : since we save entity to DB and Not the DTO
-        User savedUser = userRepo.save(UserMapper.INSTANCE.mapUserDTOToUser(userDTO));
-
-        //mapping entity back to DTO since we are returning the DTO to enduser and not the real entity
-        return UserMapper.INSTANCE.mapUserToUserDTO(savedUser);
-    }
-
-    public UserDTO getUserById(Integer userId) {
-
-        User searchedUser = userRepo.findById(userId).orElse(null);
-
-        return UserMapper.INSTANCE.mapUserToUserDTO(searchedUser);
-    }
+    public UserDTO getUserById(Integer userId);
 }
---
+++ src/main/java/com/sachinsk/user_ms/service/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package com.sachinsk.user_ms.service.impl;
+
+import com.sachinsk.user_ms.dto.UserDTO;
+import com.sachinsk.user_ms.entity.User;
+import com.sachinsk.user_ms.mapper.UserMapper;
+import com.sachinsk.user_ms.repository.UserRepo;
+import com.sachinsk.user_ms.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepo userRepo;
+
+    public UserDTO addUserToDB(UserDTO userDTO) {
+
+        //mapping DTO to entity to save to DB : since we save entity to DB and Not the DTO
+        User savedUser = userRepo.save(UserMapper.INSTANCE.mapUserDTOToUser(userDTO));
+
+        //mapping entity back to DTO since we are returning the DTO to enduser and not the real entity
+        return UserMapper.INSTANCE.mapUserToUserDTO(savedUser);
+    }
+
+    public UserDTO getUserById(Integer userId) {
+
+        User searchedUser = userRepo.findById(userId).orElse(null);
+
+        return UserMapper.INSTANCE.mapUserToUserDTO(searchedUser);
+    }
+}
