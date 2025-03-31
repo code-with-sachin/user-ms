0352d469e1486a0934dfc2c6ef6d78c64d8a7336
Sachin <sachinkaushikofficial@gmail.com>
2025-03-31 11:17:26 +0530

    Adding getters and setters as lombok not working

--- src/main/java/com/sachinsk/user_ms/entity/User.java
+++ src/main/java/com/sachinsk/user_ms/entity/User.java
@@ -4,14 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -21,4 +19,54 @@ public class User {
     private String address;
     private String city;
 
+    public User() {
+    }
+
+    public User(int userId, String userName, String userPassword, String address, String city) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.address = address;
+        this.city = city;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
